c2c46ca72cf545cd1175d45d4ed0d75e5c43ae62
ShuolBDe <554123459@qq.com>
2015-09-26 20:23:14 +0800

    p22

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var settings = require('./settings');
 var users = require('./routes/users');
 
 var app = express();
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db, //cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}, // 30 days
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port: settings.port 
+  })
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
